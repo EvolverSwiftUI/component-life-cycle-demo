c61e38d27655dec7523c37c4f1f308eb3b56b400
Sivaram Yadav <sivaramaiah.nakka@concentrix.com>
2022-06-04 20:03:33 +0530

    addons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import {Component} from 'react';
+import Clock from './components/Clock';
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {showClock: false};
+
+  onToggleClock = () => {
+    this.setState( prevState => {
+      const {showClock} = prevState;
+      return {
+          showClock: !showClock,
+        }
+    })
+  }
+
+  render() {
+    const {showClock} = this.state;
+    return(
+      <div>
+        <button onClick={this.onToggleClock}>
+          { showClock ? "Hide Clock" : "Show Clock" }
+        </button>
+        <Clock />
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/Clock/index.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+import './index.css';
+
+class Clock extends Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Clock</h1>
+            </div>
+        );
+    }
+}
+
+export default Clock;
